ea23174c8018fe2783899be67ee6332476b4f322
Patrice Thomas <patrice.thomas1229@gmail.com>
2018-08-09 18:40:44 -0400

    added click event for correct to appear when correct answer is chosen

--- main.js
+++ main.js
@@ -4,8 +4,15 @@
 funcQuestionArray= [];
 
 
-//assigning my HTLML element to a variable
+//assigning my HTLML element to a variable that holds the spot for the question
 let questionsAppear = document.getElementById('populateQuestions')
+
+//assigning my HTML elements with attribute class to a variable that will hold the list of answer choice
+let answersAppear = document.getElementsByClassName('answerChoice')
+console.log(answersAppear)
+
+//creating a variable that will hold the HTML element which displays response to user after they click
+let responseAppear = document.getElementById('response')
 
 
 
@@ -42,23 +49,33 @@ console.log(funcQuestionArray);
 //creating a function that populates question on the html page
 let questionPopulate= function(){
     // for(let i=0;i<funcQuestionArray.length;i++){    --- this for loop is needed for dynamically loop to our array of function questions objects, but I am not able to make it work right now. Insted of putting funcQuestionArray[i] below, i put funcQuestionArray[0].questions, just to make it work on the HTML page.
-    questionsAppear.innerText = funcQuestionArray[0].questions
-// }
+    
+        questionsAppear.innerText = funcQuestionArray[0].questions
 }
 questionPopulate();
 
+//creating a function to randomized the answer options (not part of the MVP)
+
 //creating a function that will populate the answer option property in the function questions object
 let answersChoices = function(){
-    
+    //again the below is working but i need to know how to run this in a loop.
+    for (let i=0;i<answersAppear.length;i++){
+    answersAppear[i].innerText=funcQuestionArray[0].answerOptions[i]
+    }
 }
+answersChoices();
+
+//creating an function for an event handler that will give a response each time a user click on an answer choice
+let correctResponse = function(){
+responseAppear.innerText= 'correct'
+}
+// correctChoice();
+
+//creating event handler to give response when correct choice is clicked
+let correct_Clicked = function(e) {
+    correctResponse();
+}   
 
 
-
-
-// //creating a test event handler
-// let  = function(){
-// alert('it is working')
-// }
-
-// //creating an code to run when the event click is fired in my HTML
-// functions.addEventlistener('click',test)
+// creating an code to run when the event click is fired in my HTML
+ answersAppear[0].addEventListener('click',correct_Clicked)
